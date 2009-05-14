33ed55e2941ee76223bbe7dd37de89cdbddd2de9
Olivier Mengué <dolmen@cpan.org>
2009-05-14 16:58:44 +0000

    Import of DOLMEN/Acme-PM-Paris-Meetings-200905.02 from CPAN.
    gitpan-cpan-distribution: Acme-PM-Paris-Meetings
    gitpan-cpan-version:      200905.02
    gitpan-cpan-path:         DOLMEN/Acme-PM-Paris-Meetings-200905.02.tar.gz
    gitpan-cpan-author:       DOLMEN
    gitpan-cpan-maturity:     released

--- lib/Acme/PM/Paris/Meetings.pm
+++ lib/Acme/PM/Paris/Meetings.pm
@@ -5,51 +5,95 @@ use strict;
 
 use DateTime::Format::ICal;
 
+use Exporter 'import';
+our @EXPORT = qw(next_meeting);
+
 =head1 NAME
 
 Acme::PM::Paris::Meetings - Get the date/time of the next Paris.pm meeting!
 
 =head1 VERSION
 
-Version 0.01
+Version 200905.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '200905.02';
 
 
 =head1 SYNOPSIS
 
+Using the 'paris-pm' script:
+    $ paris-pm -3
+    
+
 One-liner:
 
-    perl -MAcme::PM::Paris::Meetings -e "print Acme::PM::Paris::Meetings->recurrence->next"
+    perl -MAcme::PM::Paris::Meetings -e "print next_meeting"
 
 Longer:
 
     use DateTime;
     use Acme::PM::Paris::Meetings;
 
-    my $rec = Acme::PM::Paris::Meetings->recurrence();
-    my $dt = $rec->next(DateTime->now(time_zone => 'Europe/Paris'));
+    my $rec = Acme::PM::Paris::Meetings::recurrence();
+    my $dt = $rec->iterator->next(DateTime->now(time_zone => 'Europe/Paris'));
     ...
+
+=cut
+
+
 
 =head1 FUNCTIONS
 
 
 =head2 recurrence
 
-Returns a DateTime::Set from which you can get the date/time of the next Paris.pm meeting.
+Returns a new DateTime::Set from which you can get the date/time of the planned
+Paris.pm meeting for the following months.
 
 =cut
 
 sub recurrence
 {
+    my $dtstart = @_
+                ? $_[0]
+                : DateTime->now(time_zone => 'Europe/Paris',
+                                locale => 'fr_FR')
+                                ->truncate(to => 'day');
     DateTime::Format::ICal->parse_recurrence(
         recurrence => ical(),
-        dtstart => DateTime->now(time_zone => 'Europe/Paris')
-                           ->truncate(to => 'day')
+        dtstart => $dtstart,
     );
 }
+
+=head2 next_meeting
+
+Convenience function that returns the date/time of the next Paris.pm meeting
+as a string formatted for french humans.
+
+An internal recurrence object is maintained so if you call the method multiple times,
+you will get different results.
+
+=cut
+
+my $iterator;
+
+sub next_meeting
+{
+    my $count = shift(@_) || -1;
+
+    unless (defined $iterator) {
+        $iterator = recurrence()->iterator;
+    }
+
+    if (wantarray && $count >= 1) {
+        map { $iterator->next()->strftime("%A %d/%m/%Y %Hh%M") } 1..$count;
+    } else {
+        $iterator->next()->strftime("%A %d/%m/%Y %Hh%M")
+    }
+}
+
 
 =head2 ical
 
@@ -58,8 +102,17 @@ Returns the current ICal expression for the next Paris.pm meeting.
 =cut
 
 sub ical {
-	'FREQ=MONTHLY;BYDAY=2WE;BYHOUR=20;BYMINUTE=0;BYSECOND=0;COUNT=12'
+	'FREQ=MONTHLY;BYDAY=2WE;BYHOUR=20;BYMINUTE=0;BYSECOND=0'
 }
+
+
+1; # End of Acme::PM::Paris::Meetings
+__END__
+
+=head1 VERSION NUMBERS
+
+Numbering scheme: C<YYYYMM.nn> where C<YYYY> is the year, C<MM> the month and
+C<nn> the release number in the month.
 
 =head1 AUTHOR
 
@@ -70,6 +123,7 @@ Olivier MenguE<eacute>, C<< <dolmen at cpan.org> >>
 Let's hope a recent release of the module has been released and you are using it!
 
 You can still check L<http://paris.mongueurs.net> before visiting us.
+
 
 Please report any bugs or feature requests to C<bug-acme-pm-paris-meetings at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-PM-Paris-Meetings>.  I will be notified, and then you'll
@@ -117,5 +171,3 @@ under the same terms as Perl itself.
 
 
 =cut
-
-1; # End of Acme::PM::Paris::Meetings
